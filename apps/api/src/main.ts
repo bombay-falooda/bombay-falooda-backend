34c981192a59179c3c89a67c06737ffe1a3a1fe6
Zynteq Technologies <zynteqtechnologies@gmail.com>
2026-08-20 22:39:52 +0530

    Set default fallback port to 10000 for Render

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(ApiModule);
   const configService = app.get(ConfigService);
   const apiPrefix = configService.get<string>('API_PREFIX', 'api');
-  const port = process.env.PORT || configService.get<number>('API_PORT', 4000);
+  const port = process.env.PORT || configService.get<number>('API_PORT', 10000);
 
   app.use(json({ limit: '10mb' }));
   app.use(urlencoded({ extended: true, limit: '10mb' }));
